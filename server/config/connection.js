793b07d652ecc188479d823f7cab3dc3464e9ffa
Kent Ball <kent.vincent.ball@gmail.com>
2024-10-07 11:26:52 -0400

    Production updates

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
+  cache: 'bounded',
   formatError: (err) => {
     console.error('Apollo Server Error:', err);
     return err;
